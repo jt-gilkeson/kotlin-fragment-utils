436065b0d421db32a93f36d728a30395f6ec5264
J.T. Gilkeson <jt@gogobot.com>
2018-07-02 16:16:34 -0700

    Update versions, get rid of custom kotlin folder

--- kotlin-fragment-utils/src/main/java/com/jt/kotlinfragmentutils/FragmentWrapperActivity.kt
+++ kotlin-fragment-utils/src/main/java/com/jt/kotlinfragmentutils/FragmentWrapperActivity.kt
@@ -16,10 +16,10 @@
 package com.jt.kotlinfragmentutils
 
 import android.content.Context
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MenuItem
 
